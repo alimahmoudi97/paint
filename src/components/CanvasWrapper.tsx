2862d7d78490e75f67b8be55db97bd7200eb95c8
alimahmoudi97 <alimahmoudi637@gmail.com>
2025-02-09 14:54:38 +0330

    create a grid layout for canvas

--- src/components/CanvasWrapper.tsx
+++ src/components/CanvasWrapper.tsx
@@ -6,6 +6,12 @@ import PenTool from "./PenTool";
 import { EraserTool } from "./EraserTool";
 import { TextTool } from "./TextTool";
 
+const CELL_SIZE = 30;
+
+function snapToGrid(point) {
+  return Math.round(point / CELL_SIZE) * CELL_SIZE;
+}
+
 function CanvasWrapper() {
   const { contentState, setContentState } = useContextCanvas();
   const canvasRef = useRef(null);
@@ -17,6 +23,45 @@ function CanvasWrapper() {
 
     const canvas = new Canvas(canvasRef.current, { perPixelTargetFind: true });
     fabricRef.current = canvas;
+
+    canvas._renderBackground = function (ctx) {
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      const zoom = this.getZoom();
+      const offsetX = this.viewportTransform[4];
+      const offsetY = this.viewportTransform[5];
+
+      ctx.strokeStyle = "#e6fbff";
+      ctx.lineWidth = 1;
+
+      const gridSize = CELL_SIZE * zoom;
+
+      const numCellsX = Math.ceil(canvas.width / gridSize);
+      const numCellsY = Math.ceil(canvas.height / gridSize);
+
+      const gridOffsetX = offsetX % gridSize;
+      const gridOffsetY = offsetY % gridSize;
+
+      ctx.save();
+      ctx.beginPath();
+
+      for (let i = 0; i <= numCellsX; i++) {
+        const x = gridOffsetX + i * gridSize;
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, canvas.height);
+      }
+
+      for (let i = 0; i <= numCellsY; i++) {
+        const y = gridOffsetY + i * gridSize;
+        ctx.moveTo(0, y);
+        ctx.lineTo(canvas.width, y);
+      }
+
+      ctx.stroke();
+      ctx.closePath();
+      ctx.restore();
+    };
 
     canvas.setWidth(Math.floor(window.document.body.offsetWidth * 0.9));
     canvas.setHeight(Math.floor(window.innerHeight * 0.9));
