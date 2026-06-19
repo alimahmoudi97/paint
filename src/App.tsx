7be80481ae94dad9d487e6b48c881f2d0b5430c7
alimahmoudi97 <alimahmoudi637@gmail.com>
2026-06-19 17:51:03 +0330

    create landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Canvas from "./components/CanvasWrapper";
 import "./App.css";
 import Menu from "./components/Menu";
 import ContextProvider from "./context/Context";
 import Header from "./components/Header";
 import Setting from "./components/Setting";
+import LandingPage from "./components/LandingPage";
 
 function App() {
+  const [showApp, setShowApp] = useState(false);
+
+  if (!showApp) {
+    return <LandingPage onEnterApp={() => setShowApp(true)} />;
+  }
+
   return (
     <ContextProvider>
       <div className="flex h-screen">
@@ -16,7 +24,7 @@ function App() {
           <Header />
           <Canvas />
         </div>
-        <div className="bg-amber-100 flex-1">
+        <div className="flex-1">
           <Setting />
         </div>
       </div>
